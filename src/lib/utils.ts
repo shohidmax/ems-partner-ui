1e65d794d8c5d52524bb930da6c2824e0ceaf1cd
MD. Sarwar Jahan <Shohidmax@gmail.com>
2026-01-04 18:38:43 +0000

    01/4/2026 06:32:55 PM  mm/day/ yyyy  current kormat hoye ache UI te dat

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -27,27 +27,26 @@ export function formatToBDTime(dateString: string) {
   const date = getLocaleDate(dateString);
   if (!date) return dateString;
 
-  const day = date.toLocaleDateString('en-GB', { day: '2-digit', timeZone: 'Asia/Dhaka' });
-  const month = date.toLocaleDateString('en-GB', { month: 'long', timeZone: 'Asia/Dhaka' });
-  const year = date.toLocaleDateString('en-GB', { year: 'numeric', timeZone: 'Asia/Dhaka' });
-  const time = date.toLocaleTimeString('en-US', {
-      timeZone: 'Asia/Dhaka',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      hour12: true,
-  });
-
-  return `${day}/${month}/${year} ${time}`;
+  return date.toLocaleString('en-GB', {
+    timeZone: 'Asia/Dhaka',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  }).replace(',', '');
 }
 
 export function formatToBDDate(dateString: string) {
     const date = getLocaleDate(dateString);
     if (!date) return dateString;
 
-    const day = date.toLocaleDateString('en-GB', { day: '2-digit', timeZone: 'Asia/Dhaka' });
-    const month = date.toLocaleDateString('en-GB', { month: 'long', timeZone: 'Asia/Dhaka' });
-    const year = date.toLocaleDateString('en-GB', { year: 'numeric', timeZone: 'Asia/Dhaka' });
-
-    return `${day}/${month}/${year}`;
+    return date.toLocaleDateString('en-GB', {
+        timeZone: 'Asia/Dhaka',
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
 }
