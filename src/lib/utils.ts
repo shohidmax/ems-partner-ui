67055c4cd2ed275893d15dfeda8c07035a1842fa
MD. Sarwar Jahan <Shohidmax@gmail.com>
2026-01-05 07:49:27 +0000

    only device page and download pdf e mm/day/yyyy dekhaw

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -27,7 +27,7 @@ export function formatToBDTime(dateString: string) {
   const date = getLocaleDate(dateString);
   if (!date) return dateString;
 
-  return date.toLocaleString('en-GB', {
+  return date.toLocaleString('en-US', {
     timeZone: 'Asia/Dhaka',
   }).replace(',', '');
 }
@@ -36,8 +36,7 @@ export function formatToBDDate(dateString: string) {
     const date = getLocaleDate(dateString);
     if (!date) return dateString;
 
-    return date.toLocaleDateString('en-GB', {
+    return date.toLocaleDateString('en-US', {
         timeZone: 'Asia/Dhaka'
     });
 }
-
